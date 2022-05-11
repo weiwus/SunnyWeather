71ffa8d5687468a657d68736c2ebc9187dc69954
Wei JiangChao <weiwus@qq.com>
2022-05-11 21:56:36 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/weiwus/sunnyweather/ui/place/PlaceFragment.kt
@@ -0,0 +1,70 @@
+package com.weiwus.sunnyweather.ui.place
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.weiwus.sunnyweather.R
+import com.weiwus.sunnyweather.logic.model.Location
+import com.weiwus.sunnyweather.logic.model.Place
+import kotlinx.android.synthetic.main.fragment_place.*
+
+class PlaceFragment : Fragment() {
+    val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+        searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+                Log.d("PlaceFragment","content 非空${content.toString()}")
+            } else {
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+            val places = result.getOrNull()
+            if(places != null) {
+                Log.d("PlaceFragment", "places 非空，${places.toString()}")
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Log.d("PlaceFragment", "places 为空")
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+}
